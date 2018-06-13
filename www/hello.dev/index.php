69016a1fbfa51991b14839038b4ced9559c567fb
Евгений Жарко <e.zharko@reactor.com.ua>
2018-06-13 17:55:35 +0300

    test 1

--- www/hello.dev/index.php
+++ www/hello.dev/index.php
@@ -1,1 +1,15 @@
-<?php phpinfo();
+<? 
+
+#echo "$_SERVER['DOCUMENT_ROOT']<br>";
+#echo "$_ENV['PWD']";
+
+$one = 1;
+$two = 1;
+
+$res = $one + $two;
+echo "$res <br>";
+echo "Первая строка <br>";
+echo "<b>Вторая жирная строка";
+
+#phpinfo();
+?>
