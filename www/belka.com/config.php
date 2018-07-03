0a8aa5486b0e17380a638a60660a873a734b5147
evzharko <ev.zharko@gmail.com>
2018-07-04 01:04:09 +0300

    gsm report

--- www/belka.com/config.php
+++ www/belka.com/config.php
@@ -11,14 +11,10 @@ $user='root';
 $password='password';
 
 try {
-    $db = new PDO("mysql:host=$host;database=$database", $user, $password);
+    $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 
 catch (PDOException $pdoe) {
     echo $pdoe->getMessage();
-}
-
-if (!$db) {
-    echo "<br>Ошибка подключения к базе данных";
 }
