100c4e1cf916b2071c7402d78509c64617971cf5
Евгений Жарко <e.zharko@reactor.com.ua>
2018-07-06 17:26:23 +0300

    fix zabbix list

--- www/belka.com/zabbix_host.php
+++ www/belka.com/zabbix_host.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Created by PhpStorm.
@@ -14,36 +15,64 @@ $dbname = 'zabbix';
 $user = 'reactor';
 $password = 'hwth5t3gq3hawgrger';
 
+//$group = $_GET['g'] ?? null;
+
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 }
 
 catch (PDOException $error) {
     echo $error->getMessage();
 }
 
-$query = "
+$query_group = "
 SELECT 
-	*
+	DISTINCT(gr.groupid), gr.name, tf.dns, tf.ip, tf.port, INET_ATON (tf.ip) as ip1
 FROM 
-	interface
+	hosts ho
+JOIN hosts_groups hs ON hs.hostid=ho.hostid
+JOIN groups gr ON gr.groupid=hs.groupid
+JOIN interface tf ON tf.hostid=ho.hostid
 WHERE 
-	ip LIKE '192.168.%'
-";
-$result = $db->query($query);
-    echo "
+	tf.ip LIKE '192.168.%'
+ORDER BY ip1;
+	";
+
+$result_group = $db->query($query_group);
+//Берем айди группы
+$group = $_GET["g"];
+
+//Выводим список select
+echo "<select name='group' id='select'>";
+
+$array = [];
+foreach($result_group as $item_group)
+{
+    //Зансоим в масив для следующего прохода foreach
+    $array[] = $item_group;
+    //Select остаеться на той группе которая выбрана
+    $selected = $group == $item_group['groupid'] ? 'selected' : null;
+    //Для выбора группы
+    echo "<option $selected value='{$item_group['groupid']}'>{$item_group['name']}</option>";
+}
+
+
+echo "</select>";
+
+echo "
           <style type=\"text/css\">
-               Table {
+               table {
                 border: 1px solid black;
                 border-collapse: collapse;
                }
-               TD, TH {
+               td, th {
                 border: 1px #989898  solid; 
                 text-align: center; 
                 padding: 4px; 
                }
-               TH {
+               th {
                 background: beige;
                 height: 15px; 
                 vertical-align: bottom; 
@@ -55,8 +84,11 @@ $result = $db->query($query);
           <tr><th>DNS</th><th>IP</th><th>Port</th><th>NEW IP</th><tr>
 ";
 
-foreach ($result as $row) {
 
+foreach ($array as $row) {
+
+    //Сортируем данные от группы
+    if (!$group || ($group && $row['groupid'] == $group))
     echo "
            <tr>
            <td>" . $row['dns'] . "</td>
@@ -67,4 +99,15 @@ foreach ($result as $row) {
            ";
 }
     echo "</table>";
+
 ?>
+<?php
+//Скриптом ловим айдишку и далее ловим ее GET
+?>
+<script>
+    document.getElementById('select').addEventListener('change', function(e){
+        document.location.href = '?g='+e.target.value;
+    })
+</script>
+
+
