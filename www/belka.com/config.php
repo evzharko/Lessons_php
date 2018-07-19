20f7bf465d5f97d0b43a67eb8fc58206bcd051e8
evzharko <ev.zharko@gmail.com>
2018-07-19 22:44:56 +0300

    function

--- www/belka.com/config.php
+++ www/belka.com/config.php
@@ -11,7 +11,7 @@ $user='root';
 $password='password';
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 
--- www/belka.com/function/function.php
+++ www/belka.com/function/function.php
@@ -5,6 +5,35 @@
  * Date: 17.07.2018
  * Time: 22:34
  */
+include "../config.php";
+
+function Posts()
+{
+    require("../config.php");
+    $query = "
+SELECT 
+	*
+FROM
+	posts";
+
+    $stmt = $db->query($query);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach ($stmt as $item) {
+        /*$var = $item['title'];
+        $var1 = $item['body'];
+        return array('val' => $var, 'val1' => $var1);*/
+        return $item;
+    }
+
+}
+echo "<pre>";
+/*echo "<pre>";
+var_dump(Posts());*/
+//var_dump($item);
+var_dump(Posts());
+Posts();
+
 
 
 ?>
