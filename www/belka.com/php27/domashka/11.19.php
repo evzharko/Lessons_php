b574f581aa578bfae7a1253b2b729110a3b48259
evzharko <ev.zharko@gmail.com>
2018-08-16 08:06:05 +0300

    lesson

--- www/belka.com/php27/domashka/11.19.php
+++ www/belka.com/php27/domashka/11.19.php
@@ -4,23 +4,47 @@ echo "Lesson 11.19 <br><br>";
 $array = [1, 2, 3];
 $a = 20;
 echo "<pre>";
+var_dump(pow($array[2], 2));
 
-echo "Определить сумму всех элементов массива <br>";
 
-for ($i = 0; $i < 3; $i++) {
+//Генерим масив с слуйчайными числами
+for ($i = 0; $i < 7; $i++) {
     $array1[$i] = rand(0, 5);
     print $array1[$i];
 }
 echo "<br>";
 
-for ($i = 0; $i < 3; $i++) {
-    $sum += $array[$i];
+for ($i = 0; $i < 7; $i++) {
+    $sum += $array1[$i];
 }
 
-echo 'Сумма элементов масива ' . $sum;
+echo 'Сумма элементов масива ' . $sum . '<br>';
 
-/*
-foreach ($array as $key => $value) {
-    $value += $array[$key];
-    echo $value;
-}*/
+//
+$pro = 1;
+for ($i = 0; $i < 7; $i++) {
+    $pro *= $array1[$i];
+}
+
+echo 'Произведение элементов масива ' . $pro . '<br>';
+
+//
+
+for ($i = 0; $i < 7; $i++) {
+    $sumk += pow($array1[$i], 2);
+    
+}
+echo 'Сумма квадратов всех элементов масива ' . $sumk . '<br>';
+
+for ($i = 0; $i < 6; $i++) {
+    $sum6 += $array1[$i];
+    
+}
+echo 'Сумма шести первых элементов ' . $sum6 . '<br><br>';
+
+for ($i = 0; $i < 7; $i++) {
+    $sum1 += $array1[$i];
+    $avg = $sum1/7;
+}
+
+echo 'Среднее арифметическое всех элементов ' . $avg . '<br>';
---
+++ www/belka.com/php27/domashka/11.21.php
@@ -0,0 +1,10 @@
+<?php
+echo "Lesson 11.21 <br><br>";
+
+$array_j=[];
+for ($i=1; $i < 32; $i++) { 
+	//echo $i . ' ';
+
+	$array_j[] = $i;
+	var_dump($array_j);
+}
