ad323333e22974a5ccdb1bef402ac16c07f81b8a
Евгений Жарко <e.zharko@reactor.com.ua>
2018-08-29 13:26:28 +0300

    check int count function cart

--- www/php27.com/function/fun_cart.php
+++ www/php27.com/function/fun_cart.php
@@ -1,16 +1,17 @@
 <?php
 
-$count = 0;
-$price = 0;
-//$sum = 0;
 
 function cart_sum($count, $price)
 {
-    if ($count < 0 || $price < 0 || is_float($count)) {
-        echo "Цена или кол-во товара не может быть меньше 0";
+    if ($count != (int)$count) {
+        echo 'Кол-во товара должно быть равно единице';
     } else {
-        $sum = $count * $price;
-        return "Сумма покупки " . $sum;
+        if ($count <= 0 || $price <= 0) {
+            echo "Цена или кол-во товара не может быть меньше 0";
+        } else {
+            $sum = $count * $price;
+            return "Сумма покупки " . $sum;
+        }
     }
 
 }
