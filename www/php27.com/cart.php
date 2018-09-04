8d8b21dba37d56ea62d71331b36a5a1c292fdb78
Евгений Жарко <e.zharko@reactor.com.ua>
2018-09-04 18:19:22 +0300

    add cart session

--- www/php27.com/cart.php
+++ www/php27.com/cart.php
@@ -1,7 +1,10 @@
 <?php
 require_once "function/fun_cart.php";
 echo $_SESSION['login'];
-session_start();
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = ['sum' => 0, 'items' => []];
+}
+
 
 ?>
     <html xmlns="http://www.w3.org/1999/html">
@@ -18,6 +21,7 @@ session_start();
             Кол-во товара <input type="number" min="0" name="count" required="required"/> <br><br>
             Цена товара <input type="text" name="price" required="required"/> <br><br>
             <button type="submit" name="submit">Посчитать сумму</button>
+            <br><br> К оплате <?php $_SESSION['cart']['sum']; ?>
             <br><br>
         </form>
     </div>
@@ -27,7 +31,8 @@ session_start();
 
 <?php
 
-if (isset($_POST['submit'])) {
+/*if (isset($_POST['submit'])) {
     echo cart_sum($_POST['count'], $_POST['price']);
-}
+}*/
+echo '<pre>';
 
