02721ce397db710edc64e33a193f3144013c38f1
evzharko <ev.zharko@gmail.com>
2018-11-10 12:31:22 +0200

    Update Connect.php

--- www/php27.com/Connect.php
+++ www/php27.com/Connect.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Евгений
- * Date: 07.11.2018
- * Time: 20:44
+ * User: student
+ * Date: 10.11.2018
+ * Time: 9:41
  */
 
 class Connect
@@ -15,75 +15,145 @@ class Connect
     public $database;
 
     /**
-     * @param $host
-     * @param $login
-     * @param $password
-     * @param $database
      * @return mysqli
      */
-    public static function getInstance($host, $login, $password, $database)
+    public static function getInstance()
     {
         if (self::$link) {
             return self::$link;
         }
-        self::$link = mysqli_connect($host, $login, $password, $database);
+        self::$link = mysqli_connect('127.0.0.1', 'root', '', 'php27');
 
-        if(mysqli_connect_error(self::$link)){
+        if (mysqli_connect_error(self::$link)) {
             echo "Не удалось подключиться к MySQL " . mysqli_connect_error();
         }
         return self::$link;
     }
 }
 
-class Qb extends Connect
+class Qb
 {
+    /** @var  mysqli */
+    private $db;
+    private $sql;
+    private $select;
+    private $from;
+    private $where = 'WHERE 1=1 ';
+    protected $table_name = '';
 
-    public function execute($sql)
+    /**
+     * @return mixed
+     */
+    public function getDb()
     {
-        $login = 'root';
-        $host = 'mysql';
-        $password = 'password';
-        $database = 'php27';
-
-        $stmt = self::getInstance($host, $login, $password, $database)->prepare($sql);
-        //var_dump('<pre>',$stmt->execute());
-        return $stmt->execute();
+        return $this->db;
     }
 
-    public function select($sql)
+    /**
+     * @param mixed $db
+     */
+    public function setDb()
     {
-        $exe = $this->execute($sql);
+        $this->db = Connect::getInstance();
 
-        var_dump($sql);
-        $result = mysqli_fetch_all($exe, MYSQLI_ASSOC);
+    }
 
-        if($result === false){
-            return [];
+    /**
+     * Qb constructor.
+     */
+    public function __construct()
+    {
+        $this->setDb();
+
+        $this->from(strtolower(get_class($this)) . 's');
+    }
+
+
+    public function get()
+    {
+
+        if (empty($this->select)) {
+            $this->select();
+        }
+        $this->sql = $this->select . $this->from;
+//        var_dump($this->sql);die();
+        $stmt = $this->db->stmt_init();
+        $stmt->prepare($this->sql);
+        $stmt->execute();
+        /* привязываем результаты к переменным */
+        $res = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        foreach ($res as $rowkey => $row) {
+            foreach ($row as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+        return true;
+    }
+
+
+    public function first()
+    {
+
+        if (empty($this->select)) {
+            $this->select();
+        }
+        $this->sql = $this->select . $this->from .' LIMIT 1 ';
+        $stmt = $this->db->stmt_init();
+        $stmt->prepare($this->sql);
+        $stmt->execute();
+        /* привязываем результаты к переменным */
+        $res = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        foreach ($res as $rowkey => $row) {
+            foreach ($row as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+        return true;
+    }
+    public function select(array $fields = [])
+    {
+        $this->select = 'SELECT ';
+        if (empty($fields)) {
+            $this->select .= '* ';
+            return $this;
         }
 
-        return $result;
+        $this->select .= implode(',', $fields);
 
+        return $this;
     }
 
-    public function update()
+    public function from($table)
     {
+        $this->from = ' FROM ' . $table;
 
+        return $this;
     }
 
-    public function insert()
+    public function where(array $q)
     {
 
-    }
-
-    public function delete()
-    {
-
+        foreach ($q as $key => $value) {
+            $this->where = $key . '= ' . $value;
+        }
     }
 }
 
-$db = new Qb();
-$products = $db->select("Select * From products");
-echo $products;
+class User extends Qb
+{
+//    protected $table_name = 'users';
+
+public function getInfo(){
+    return $this->name.' '. $this->phone;
+}
+}
+
+$user = new User();
+
+$user->first();
+
+echo $user->getInfo();
+//echo $products;
 
 /*$select = $db->select("Select * From products");
 foreach ($select as $item) {
