7b33d6d8ac41fe368221f74351e561df1a0e837e
evzharko <ev.zharko@gmail.com>
2018-11-17 18:47:02 +0200

    install debuger

--- www/belka.com/php27/domashka/11.28.php
+++ www/belka.com/php27/domashka/11.28.php
@@ -1,26 +1,4 @@
-<?php
-echo "Lesson 11.28 <br><br>";
+$query_select = "SELECT * FROM sip_peers ORDER BY name";
 
-$day = 3;
-
-echo "<pre>";
-
-	for ($i = 0; $i < $day; $i++) { 
-	$array_j[$i] = rand(10,-10);
-}
-
-var_dump($array_j);
-
-$sum = 0; 
-
-foreach ($array_j as $value) {
-	$sum += $value;
-}
-
-if ($sum > 0) {
-	$polozhitelnoe = "Сумма элементов масима положительная $sum";
-} else {
-	$polozhitelnoe = "Сумма элементов масима отрицательная $sum";
-}
-
-echo $polozhitelnoe;
+$res = mysqli_query($link_asterisk, $query_select);
+$row_peers = mysqli_fetch_all($res, MYSQLI_ASSOC);
---
+++ www/php27.com/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//echo  phpinfo();
+
+$a = 8;
+$b = 1;
+
+$c = $a + $b;
+
+echo $c;
+
