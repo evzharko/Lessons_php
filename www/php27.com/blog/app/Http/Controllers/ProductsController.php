ca514f6b84632c0ec86870e6f78a341feac801c5
ev.zharko@gmail.com <Kewpdvmyu9x>
2018-12-16 18:10:06 +0200

    add post products

--- www/php27.com/blog/app/Http/Controllers/ProductsController.php
+++ www/php27.com/blog/app/Http/Controllers/ProductsController.php
@@ -25,7 +25,7 @@ class ProductsController extends Controller
 //        return view('user.edit', ['user' => $user]);
     }
 
-    public function create()
+    public function add()
     {
         return view('products.create');
     }
