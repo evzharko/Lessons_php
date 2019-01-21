cb5743c25b3eeee6aec7245e4c992f810b954e0f
Евгений Жарко <e.zharko@reactor.com.ua>
2019-01-21 19:30:19 +0200

    edit products

--- www/php27.com/blog/app/Http/Controllers/Products/ProductsController.php
+++ www/php27.com/blog/app/Http/Controllers/Products/ProductsController.php
@@ -53,6 +53,7 @@ class ProductsController extends Controller
     {
 
         $product = Product::find($id);
+        dd(Product::find($id));
         $product->slug = $request->get('ProductSlug');
         $product->name = $request->get('ProductName');
         $product->description = $request->get('ProductDescription');
@@ -60,7 +61,6 @@ class ProductsController extends Controller
         $product->category_id = $request->get('ProductCategory');
         $product->price = $request->get('ProductPrice');
         $product->img = $request->get('ProductImg');
-//        dd($product->save());
         $product->save();
         return redirect(route('products.edit', ['product' => $product]));
     }
