51bfc217ac282633e0249b0a04784b121c5a6a0b
Евгений Жарко <e.zharko@reactor.com.ua>
2019-02-19 16:10:15 +0200

    mn,

--- www/php27.com/blog/app/Http/Controllers/Products/ManufactureController.php
+++ www/php27.com/blog/app/Http/Controllers/Products/ManufactureController.php
@@ -7,6 +7,7 @@ use App\Manufacturer;
 use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Http\File;
 
 class ManufactureController extends Controller
 {
@@ -25,8 +26,18 @@ class ManufactureController extends Controller
         return view('admin.manufacture.create');
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function PostCreate(Request $request)
     {
+        $Path_Save = 'upload/images';
+        $NewNameImg = $request->post('ManufactureName');
+        $PathName = $request->file('ManufactureImg')->move($Path_Save, $NewNameImg);
+        $img_url = $Path_Save;
+        dd($img_url);
+
         $ManufactureName = new Manufacturer();
         $ManufactureName->name = $request->post('ManufactureName');
         $ManufactureName->save();
